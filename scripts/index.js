d10f333109d442de9d340cca2bf5f15d1a1a01a7
Dmitry Buyanov <buyanov-dmitriy@yandex.ru>
2021-06-11 20:30:25 +0200

    After first review

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,34 @@
-let NameOfAuthor = document.querySelector('.profile__name');
-let DescriptionOfAuthor = document.querySelector('.profile__description');
-const PopupOpenButton = document.querySelector('.profile__edit-button');
+let nameOfAuthor = document.querySelector('.profile__name');
+let descriptionOfAuthor = document.querySelector('.profile__description');
+const popupOpenButton = document.querySelector('.profile__edit-button');
 
-let Popup = document.querySelector('.popup');
-let formElement = Popup.querySelector('.popup__container');
-let PopupNameOfAuthor = Popup.querySelector('.popup__name-of-author');
-let PopupDescriptionOfAuthor = Popup.querySelector('.popup__description-of-author');
-const PopupCloseButton = Popup.querySelector('.popup__close');
+let popup = document.querySelector('.popup');
+let formElement = popup.querySelector('.popup__container');
+let popupNameOfAuthor = popup.querySelector('#name');
+let popupDescriptionOfAuthor = popup.querySelector('#description');
+const popupCloseButton = popup.querySelector('.popup__close');
 
 
-const OpenPopup = function() {
-  Popup.classList.add('popup_active');
+const openPopup = function() {
+  popupNameOfAuthor.value = nameOfAuthor.textContent;
+  popupDescriptionOfAuthor.value = descriptionOfAuthor.textContent;
+
+  popup.classList.add('popup_opened');
 }
 
-const ClosePopup = function() {
-  Popup.classList.remove('popup_active');
+const closePopup = function() {
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  NameOfAuthor.textContent = PopupNameOfAuthor.value;
-  DescriptionOfAuthor.textContent = PopupDescriptionOfAuthor.value;
+  nameOfAuthor.textContent = popupNameOfAuthor.value;
+  descriptionOfAuthor.textContent = popupDescriptionOfAuthor.value;
 
-  ClosePopup();
+  closePopup();
 }
 
-PopupOpenButton.addEventListener('click', OpenPopup);
-PopupCloseButton.addEventListener('click', ClosePopup);
-
-PopupNameOfAuthor.value = NameOfAuthor.textContent;
-PopupDescriptionOfAuthor.value = DescriptionOfAuthor.textContent;
-
+popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
